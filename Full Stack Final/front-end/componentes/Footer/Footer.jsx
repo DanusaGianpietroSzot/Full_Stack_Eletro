677da166aa8218453759710ea1363096a2f1a393
Danusa Gianpietro Szot Costa <73791493+DanusaGianpietroSzot@users.noreply.github.com>
2021-02-26 19:20:31 -0300

    Add files via upload

---
+++ Full Stack Final/front-end/componentes/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="container">
+            <p>Formas de Pagamento</p>
+            <img src="./imagens/formasDePagamento.jpeg" alt="Formas de pagamento" />
+            <p className="text-muted">&copy; RecodePro</p>
+        </footer>
+    );
+} 
